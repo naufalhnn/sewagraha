82bbe68ee9fd14f05d1b1e0a4a3821b9da25acba
naufalhnn <naufalhanan92@gmail.com>
2025-06-09 23:45:40 +0700

    fix test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,39 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Spatie\Permission\PermissionRegistrar; // Impor kelas untuk Spatie
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
+
+    /**
+     * Boots the application.
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__ . '/../bootstrap/app.php';
+
+        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+
+        return $app;
+    }
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Reset Spatie's permission cache before each test
+        $this->app->make(PermissionRegistrar::class)->forgetCachedPermissions();
+    }
 }
